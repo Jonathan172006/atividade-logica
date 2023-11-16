55e092b577129eb266ca6bfad44cb70f72d84151
Jonathan172006 <143630416+Jonathan172006@users.noreply.github.com>
2023-11-16 13:32:22 -0300

    Add files via upload

---
+++ exercício8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL,"");
+
+    int i;
+    int quantidade = 6;
+    int numeros[quantidade];
+
+   do {
+
+    for (i = 0; 6 < quantidade; i++);
+    {
+      printf("Digite o %dº numero: \n", i+1);
+      scanf("%d",&numeros[i]);
+    }
+
+    if ()
+
+
+      return 0;
+    }
